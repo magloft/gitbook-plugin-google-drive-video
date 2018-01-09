22cf23007ca9e1b9d1edd0ee140f28fd74d6d0da
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2018-01-09 20:38:41 +0800

    fix google drive thumbnail url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
         if (videoID == null) { return '' }
         const shareUrl = `https://drive.google.com/file/d/${videoID}/view`
         const embedUrl = `//drive.google.com/file/d/${videoID}/preview`
-        const thumbUrl = `https://lh3.google.com/u/0/d/${videoID}=w1280-h720-p-k-nu-iv1`
+        const thumbUrl = `https://drive.google.com/thumbnail?sz=w1024&id=${videoID}`
         if (this.generator === 'website') {
           return `<div style="position: relative; padding-bottom: 56.25%; padding-top: 25px; height: 0;">\n` +
                  `  <iframe src="${embedUrl}" style="border: none; position: absolute; top: 0; left: 0; width: 100%; height: 100%;" frameborder="0" allowfullscreen></iframe>\n` +
